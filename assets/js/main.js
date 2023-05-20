ca631d873bda248dba0a5471b1a795e9fba44ffe
unknown <alaminbinmonsur@gmail.com>
2023-05-20 11:50:07 +0600

    modified

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,9 @@
 /*-----------------------------------------------------------------------------
-Theme Name: staco
+Theme Name: Pepe
 Author: uigigs
 Author URL: https://themeforest.net/user/uigigs/portfolio
 -----------------------------------------------------------------------------*/
 (function ($) {
-    /*-- sticky header scripts start --*/
-    // $(window).on('scroll', function () {
-    //     if ($(this).scrollTop() > 20) {
-    //         $('.header-section').addClass("sticky");
-    //     }
-    //     else {
-    //         $('.header-section').removeClass("sticky");
-    //     }
-    // });
-    /*-- sticky header scripts end --*/
-
     $('.counter').counterUp({
         delay: 10,
         time: 1000
